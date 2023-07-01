6f55898e8de0bb436a858d6f3bba8c88973aedf1
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-01 17:52:31 +0200

    playing with objects in js'

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 console.log('Ey!');
-let name = 'susan';
-console.log(name);
+
+let colour = 'blue';
+console.log(colour);
+
+let person = {
+	name: 'susan',
+	age: '10'
+};
+console.log(person);
+
+//dot notation
+person.name = 'notsusan';
+console.log(person);
+
+//bracket notation
+person['name'] = 'mary';
+console.log(person);
+//i fu dont know the name of the target property
+let selection = 'name';
+person[selection] = 'pedro';
+console.log(person);
