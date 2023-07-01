27e96c6666452e3df9376dbadf2a681f0eb1564b
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-01 18:51:47 +0200

    subdir

---
+++ js/index.js
@@ -0,0 +1,60 @@
+console.log('Ey!');
+
+//VARIABLES
+let colour = 'blue';
+console.log(colour);
+
+//OBJECTS
+
+let person = {
+	name: 'susan',
+	age: '10'
+};
+console.log(person);
+
+	//dot notation
+person.name = 'notsusan';
+console.log(person);
+
+	//bracket notation
+person['name'] = 'mary';
+console.log(person);
+
+	//dynamic access to target property
+let selection = 'name';
+person[selection] = 'pedro';
+console.log(person);
+
+//ARRAYS
+let selectedColours = ['red', 'blue'];
+console.log(selectedColours);
+console.log(selectedColours[0]);
+
+	//expanding array 
+selectedColours[2] = 'green';
+console.log(selectedColours);
+
+	//expanding array with another type
+selectedColours[3] = 1 ;
+console.log(selectedColours);
+
+	//using length property
+console.log(selectedColours.length);
+
+//FUNCTIONS
+
+	//performing a task
+function greet(name){
+	console.log('Hello ' + name);
+}
+greet('relu');
+greet('kris');
+	
+	//calculating a value
+function square(number){
+	return number * number;
+}
+
+let number = square(2);
+console.log(number);
+console.log(square(5));
