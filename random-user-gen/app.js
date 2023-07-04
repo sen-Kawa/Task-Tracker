769f345e7c7f2885b63f7a027bfe7d0de4bd29fa
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-04 19:32:44 +0200

    handling event on button

--- random-user-gen/app.js
+++ random-user-gen/app.js
@@ -8,6 +8,11 @@ const app = Vue.createApp({
 			picture: 'https://randomuser.me/api/portraits/men/10.jpg',
 		}
 	},
+	methods: {
+		getUser() {
+			console.log(this.firstName);
+		},
+	},
 })
 
 app.mount('#app')
