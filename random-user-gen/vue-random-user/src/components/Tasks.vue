80b54f8273c6753d3d58ae877c133b9b4d373915
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-05 15:12:13 +0200

    tasks component a vfor loop adding bind to id

---
+++ random-user-gen/vue-random-user/src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+	<div v-for="(task, id) in tasks" :key="id">
+		<h3>{{ task.text }}</h3>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TasksApp',
+		props: {
+			tasks: Array,
+		}
+	}
+</script>
