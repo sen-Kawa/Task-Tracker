f89ddf41bda5f83b9fb8d56a50bee4c9244888d1
olin_tp_ar <universalexperimental@protonmail.com>
2023-07-06 13:56:45 +0200

    adding task is persistent, added to db json

--- random-user-gen/vue-random-user/src/App.vue
+++ random-user-gen/vue-random-user/src/App.vue
@@ -35,8 +35,17 @@ export default {
 		toggleAddTask() {
 			this.showAddTask = !this.showAddTask
 		},
-		addTask(task) {
-			this.tasks = [...this.tasks, task]
+		async addTask(task) {
+			const res = await fetch('api/tasks', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify(task),
+			})
+			const data= await res.json()
+
+			this.tasks = [...this.tasks, data]
 		},
 		deleteTask(id) {
 			if (confirm('Are you sure?')) {
@@ -47,7 +56,12 @@ export default {
 			this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
 		},
 		async fetchTasks() {
-			const res = await fetch('http://localhost:5000/tasks')
+			const res = await fetch('api/tasks')
+			const data = await res.json()
+			return data
+		},
+		async fetchTask(id) {
+			const res = await fetch(`api/tasks/${id}`)
 			const data = await res.json()
 			return data
 		},
